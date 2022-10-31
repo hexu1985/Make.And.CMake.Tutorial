4aa5e8d475cd763d33e3dbb12ad21b8347183527
hexu <hexu_1985@sina.com>
2022-10-31 17:12:25 +0800

    rename hello_world -> hello

--- single_directory/hello/cxx/hello.cpp
+++ single_directory/hello/cxx/hello.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 
-std::string say_hello() { return std::string("Hello, CMake world!"); }
+std::string say_hello() { return std::string("Hello, world!"); }
 
 int main() {
 #ifndef NDEBUG
