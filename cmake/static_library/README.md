9ee3b5b53f00560e8eb33e3a9f1018ee98568774
hexu <hexu_1985@sina.com>
2024-02-27 16:05:26 +0800

    add static_library

---
+++ cmake/static_library/README.md
@@ -0,0 +1,2 @@
+CMake从入门到精通（二）静态库static library
+https://zhuanlan.zhihu.com/p/392739176
---
+++ cmake/static_library/cxx/src/main.cpp
@@ -0,0 +1,10 @@
+#include "Hello.h"
+
+int main(int argc, char *argv[])
+{
+    Hello hi;
+    hi.print();
+    return 0;
+}
+
+
